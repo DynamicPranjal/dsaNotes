55a2cd292e9ded66d5bd09a10e32c63d11ab5f45
Pranjal Tiwari <61671979+DynamicPranjal@users.noreply.github.com>
2022-02-17 19:40:50 +0530

    gfg

---
+++ binarySearchTrees/preorderToPostorder.cpp
@@ -0,0 +1,19 @@
+Node *makeTree(Node *root,int x){
+    if(root==NULL)
+        return newNode(x);
+    if(x<root->data)
+        root->left=makeTree(root->left,x);
+    else
+        root->right=makeTree(root->right,x);
+    return root;
+}
+
+Node* post_order(int pre[], int size)
+{
+    //code here
+    Node *root=NULL;
+    for(int i=0;i<size;i++){
+        root=makeTree(root,pre[i]);
+    }
+    return root;
+}
