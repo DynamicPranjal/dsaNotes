d21c29c36f2a24c879db596e8938d5adefcdb887
Pranjal Tiwari <61671979+DynamicPranjal@users.noreply.github.com>
2022-02-18 09:42:05 +0530

    leetcode 301

---
+++ backtracking/removeInvalidParentheses.cpp
@@ -0,0 +1,49 @@
+//remove the minimum number of invalid parentheses to make the input string valid
+
+class Solution {
+public:
+    void func(string s,vector<string> &ans,unordered_map<string,bool> &m,int rem){
+        if(m[s])
+            return ;
+        
+        m[s]=true;
+        
+        if(rem==0){
+            int removals_needed=find_rem(s);
+            if(removals_needed==0)
+                ans.push_back(s);
+            return ;
+        }
+        
+        
+        for(int i=0;i<s.size();i++){
+            string left=s.substr(0,i);
+            string right=s.substr(i+1);
+            string str=left+right;
+            func(str,ans,m,rem-1);
+        }
+}
+    
+    int find_rem(string s){
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')
+                st.push('(');
+            else if(s[i]==')'){
+                if(st.size()!=0 and st.top()=='(') 
+                    st.pop();
+                else st.push(')');
+            }
+        }
+        return st.size();
+    }
+    
+    vector<string> removeInvalidParentheses(string s) {
+        
+        unordered_map<string,bool>m;
+        int min_rem=find_rem(s);
+        vector<string>ans;
+        func(s,ans,m,min_rem);
+        return ans;
+    }
+};
